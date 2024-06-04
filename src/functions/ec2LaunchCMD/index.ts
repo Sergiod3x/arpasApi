52eaa91a5bb602fb0f5306978e1751ee4003848a
Alberto Aresu <albertoaresu@MacBook-Pro-di-Alberto-2.local>
2024-06-04 09:27:36 +0200

    delete some useless code

--- src/functions/ec2LaunchCMD/index.ts
+++ src/functions/ec2LaunchCMD/index.ts
@@ -1,21 +1,9 @@
 import { handlerPath } from '@libs/handler-resolver';
-// import type { AWS } from "@serverless/typescript";
-// import * as path from 'path';
-// const fs = require('fs')
-
-// const privateKeyPath = path.resolve(__dirname, '../../keys/key.pem');
-// const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
-
-
-// console.log("${cf:${self:service}-${self:provider.stage}-output.demoStack-prod-ec2xPrivateKeyC}")
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.call`,
   environment: {
     ACCOUNT_ID: "${aws:accountId}",
-    // sshPrivateKey: AWS,
-    // sshPrivateKey: privateKey,
-    // sshPrivateKey: "${cf:${self:service}-${self:provider.stage}-output.demoStack-prod-ec2xPrivateKeyB}",
     sshPrivateKey: "${cf:demoStack-prod-output.ec2xPrivateKeyC}",
   },
   events: [
